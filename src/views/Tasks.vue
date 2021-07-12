8ef63edaebc980fe9adfe3d092962f7f90871af5
tupic98 <raalvarenga423@gmail.com>
2021-07-12 15:11:10 -0700

    :sparkles: Add tasks view

---
+++ src/views/Tasks.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="min-h-full">
+    <task-list :list="taskList" />
+  </div>
+</template>
+<script>
+import { reactive } from "vue";
+import TaskList from "@/components/TaskList.vue";
+
+export default {
+  components: {
+    TaskList,
+  },
+  setup() {
+    const taskList = reactive([
+      {
+        id: 1,
+        name: "Develop the Home page",
+        description:
+          "For the home page, I need to do blah blah blah, and also blah blah blah to make it work and blah blah blah",
+        created_at: "06/30/2021",
+        completed: false,
+      },
+      {
+        id: 2,
+        name: "Learning Tailwind and Vue 3",
+        description:
+          "To learn these skills, I need to do blah blah blah, and also blah blah blah to make it work and blah blah blah",
+        created_at: "06/30/2021",
+        completed: false,
+      },
+      {
+        id: 3,
+        name: "Learning Tailwind and Vue 3",
+        description:
+          "To learn these skills, I need to do blah blah blah, and also blah blah blah to make it work and blah blah blah",
+        created_at: "06/30/2021",
+        completed: false,
+      },
+      {
+        id: 4,
+        name: "Learning Tailwind and Vue 3",
+        description:
+          "To learn these skills, I need to do blah blah blah, and also blah blah blah to make it work and blah blah blah",
+        created_at: "07/12/2021",
+        completed: true,
+      },
+    ]);
+
+    return {
+      taskList,
+    };
+  },
+};
+</script>
