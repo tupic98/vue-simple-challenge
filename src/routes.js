2d10f7bae969723a85ff673d60b260620b343791
tupic98 <raalvarenga423@gmail.com>
2021-07-12 15:10:48 -0700

    Add task route

--- src/routes.js
+++ src/routes.js
@@ -7,4 +7,10 @@ export const routes = [
     component: Home,
     meta: { title: 'Home' }
   },
+  {
+    path: '/tasks',
+    name: 'Tasks',
+    component: () => import(/* webpackChunkName: "tasks-view" */ './views/Tasks.vue'),
+    meta: { title: 'Tasks List' }
+  }
 ]
