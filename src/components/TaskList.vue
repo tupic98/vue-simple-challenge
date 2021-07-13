21bf3f3526030f2567729dcc8ce8039b44b9382d
tupic98 <raalvarenga423@gmail.com>
2021-07-13 01:20:28 -0700

    :lipstick: Better task view

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="w-full bg-white mt-5 shadow rounded-xl">
+  <div
+    class="w-11/12 mx-auto sm:mx-0 sm:w-full bg-white mt-5 shadow rounded-xl"
+  >
     <ul class="divide-y divide-gray-200">
       <task
         v-for="(task, i) in list"
@@ -7,6 +9,7 @@
         class="first:rounded-t-xl last:rounded-b-xl"
         :task="task"
         @open:detail="openDetail"
+        @open:delete="openDelete"
       />
     </ul>
     <detail-modal
@@ -14,17 +17,25 @@
       :task="currentTask"
       @close="closeDetailModal()"
     />
+    <delete-modal
+      :open="isDeleteOpen"
+      :task="currentTask"
+      @close="closeDeleteModal()"
+      @delete="deleteTask"
+    />
   </div>
 </template>
 <script>
-import { reactive, ref } from "vue";
+import { ref } from "vue";
 import Task from "@/components/Task.vue";
 import DetailModal from "@/components/DetailModal.vue";
+import DeleteModal from "@/components/DeleteModal.vue";
 
 export default {
   components: {
     Task,
     DetailModal,
+    DeleteModal,
   },
   props: {
     list: {
@@ -32,9 +43,10 @@ export default {
       default: () => [],
     },
   },
-  setup(props) {
-    const isDetailOpen = ref(false);
+  setup(props, { emit }) {
     const currentTask = ref({});
+
+    const isDetailOpen = ref(false);
 
     const openDetail = (id) => {
       isDetailOpen.value = true;
@@ -45,11 +57,31 @@ export default {
       isDetailOpen.value = false;
     };
 
+    const isDeleteOpen = ref(false);
+
+    const openDelete = (id) => {
+      isDeleteOpen.value = true;
+      currentTask.value = props.list.find((t) => t.id === id);
+    };
+
+    const closeDeleteModal = () => {
+      isDeleteOpen.value = false;
+    };
+
+    const deleteTask = (id) => {
+      emit("delete:task", id);
+      isDeleteOpen.value = false;
+    };
+
     return {
       isDetailOpen,
+      isDeleteOpen,
       currentTask,
       openDetail,
       closeDetailModal,
+      openDelete,
+      closeDeleteModal,
+      deleteTask,
     };
   },
 };
--- src/views/Tasks.vue
+++ src/views/Tasks.vue
@@ -1,10 +1,70 @@
 <template>
   <div class="min-h-full">
-    <task-list :list="taskList" />
+    <label for="search" class="sr-only">Search</label>
+    <div
+      class="
+        relative
+        rounded-md
+        shadow-sm
+        mt-4
+        w-11/12
+        mx-auto
+        sm:mx-0
+        sm:w-8/12
+        md:w-7/12
+        lg:w-5/12
+        xl:w-3/12
+      "
+    >
+      <div
+        class="
+          absolute
+          inset-y-0
+          left-0
+          pl-3
+          flex
+          items-center
+          pointer-events-none
+        "
+      >
+        <!-- Heroicon name: solid/search -->
+        <svg
+          class="h-5 w-5 text-gray-400"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          aria-hidden="true"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+            clip-rule="evenodd"
+          />
+        </svg>
+      </div>
+      <input
+        v-model="search"
+        type="search"
+        name="search"
+        id="search"
+        class="
+          focus:ring-pink-500
+          focus:border-pink-500
+          block
+          w-full
+          pl-10
+          sm:text-sm
+          border-gray-300
+          rounded-md
+        "
+        placeholder="Search"
+      />
+    </div>
+    <task-list :list="filteredTaskList" @delete:task="deleteTask" />
   </div>
 </template>
 <script>
-import { reactive } from "vue";
+import { ref, computed } from "vue";
 import TaskList from "@/components/TaskList.vue";
 
 export default {
@@ -12,7 +72,7 @@ export default {
     TaskList,
   },
   setup() {
-    const taskList = reactive([
+    let taskList = ref([
       {
         id: 1,
         name: "Develop the Home page",
@@ -23,7 +83,7 @@ export default {
       },
       {
         id: 2,
-        name: "Learning Tailwind and Vue 3",
+        name: "Learning Tailwind and Nuxt",
         description:
           "To learn these skills, I need to do blah blah blah, and also blah blah blah to make it work and blah blah blah",
         created_at: "06/30/2021",
@@ -31,7 +91,7 @@ export default {
       },
       {
         id: 3,
-        name: "Learning Tailwind and Vue 3",
+        name: "Learning Tailwind and Vue 2",
         description:
           "To learn these skills, I need to do blah blah blah, and also blah blah blah to make it work and blah blah blah",
         created_at: "06/30/2021",
@@ -47,8 +107,29 @@ export default {
       },
     ]);
 
+    const search = ref("");
+
+    const filteredKeys = ["name", "description"];
+
+    const filteredTaskList = computed(() =>
+      taskList.value.filter((task) =>
+        Object.keys(task).some((key) => {
+          if (filteredKeys.includes(key)) {
+            return task[key].toLowerCase().includes(search.value.toLowerCase());
+          }
+        })
+      )
+    );
+
+    const deleteTask = (id) => {
+      taskList.value = taskList.value.filter((t) => t.id !== id);
+    };
+
     return {
       taskList,
+      deleteTask,
+      search,
+      filteredTaskList,
     };
   },
 };
